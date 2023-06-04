7f053ecf5f2f507e496a4646e33073717dd27198
Lira Guedes <liracrg@gmail.com>
2023-06-04 17:48:44 -0300

    Requisitos 1 e 2

--- src/tests/About.test.js
+++ src/tests/About.test.js
@@ -1,1 +1,27 @@
-test('', () => {});
+import { screen } from '@testing-library/react';
+import React from 'react';
+import About from '../pages/About';
+import renderWithRouter from '../renderWithRouter';
+
+describe('', () => {
+  test('Teste se a página contém as informações sobre a Pokédex', () => {
+    renderWithRouter(<About />);
+    expect(screen.getByText(/details for each one/i)).toBeInTheDocument();
+  });
+  test('Teste se a página contém um heading h2 com o texto About Pokédex', () => {
+    renderWithRouter(<About />);
+    expect(screen.getByRole('heading', { name: 'About Pokédex' }, { level: 2 })).toHaveTextContent(/about pokédex/i);
+  });
+  test('Teste se a página contém dois parágrafos com texto sobre a Pokédex', () => {
+    renderWithRouter(<About />);
+    expect(screen.getByText(/This application simulates/i)).toBeInTheDocument();
+    expect(screen.getByText(/One can filter Pokémon/i)).toBeInTheDocument();
+  });
+  test('Teste se a página contém a imagem de uma Pokédex', () => {
+    renderWithRouter(<About />);
+    expect(screen.getByAltText(/pokédex/i)).toHaveAttribute(
+      'src',
+      'https://cdn2.bulbagarden.net/upload/thumb/8/86/Gen_I_Pok%C3%A9dex.png/800px-Gen_I_Pok%C3%A9dex.png',
+    );
+  });
+});
--- src/tests/App.test.js
+++ src/tests/App.test.js
@@ -1,1 +1,33 @@
-test('', () => {});
+import { screen } from '@testing-library/react';
+import userEvent from '@testing-library/user-event';
+import { act } from 'react-dom/test-utils';
+import React from 'react';
+import App from '../App';
+import renderWithRouter from '../renderWithRouter';
+
+describe('Teste se o topo da aplicação contém um conjunto fixo de links de navegação', () => {
+  test('Teste se a aplicação é redirecionada para a página inicial,', () => {
+    const { history } = renderWithRouter(<App />);
+    const screenHome = screen.getByRole('link', { name: 'Home' });
+    userEvent.click(screenHome);
+    expect(history.location.pathname).toBe('/');
+  });
+  test('Teste se a aplicação é redirecionada para a página de About', () => {
+    const { history } = renderWithRouter(<App />);
+    const screenAbout = screen.getByRole('link', { name: 'About' });
+    userEvent.click(screenAbout);
+    expect(history.location.pathname).toBe('/about');
+  });
+  test('Teste se a aplicação é redirecionada para a página de Pokémon Favoritados', () => {
+    const { history } = renderWithRouter(<App />);
+    const screenFavorites = screen.getByRole('link', { name: 'Favorite Pokémon' });
+    userEvent.click(screenFavorites);
+    expect(history.location.pathname).toBe('/favorites');
+  });
+  test('Teste se a aplicação é redirecionada para a página de Pokémon Favoritados', () => {
+    const { history } = renderWithRouter(<App />);
+    act(() => { history.push('/gfhdhfjh'); });
+    const screenUnknow = screen.getByRole('heading', { name: /Not Found/i });
+    expect(screenUnknow).toBeInTheDocument();
+  });
+});
